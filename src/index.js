d7d1e167480ed7af0016c5bd5d48636f75193b05
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-05-05 15:18:19 +0300

    coomit work

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import menuItemTpl from './templates/menu-item.hbs';
+import menu from './menu.json';
+
+const checkbox = document.getElementById('theme-switch-toggle');
+const body = document.querySelector('body');
+
+checkbox.addEventListener('change', changeTheme);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+if(localStorage.getItem('my-theme')) {
+  body.classList.add(localStorage.getItem('my-theme'));
+} else {
+  body.classList.add(Theme.LIGHT);
+}
+
+if (localStorage.getItem('my-theme') === Theme.DARK) {
+  checkbox.checked = true;
+}
+
+function changeTheme() {
+  if (checkbox.checked) {
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('my-theme', Theme.DARK);
+  } else {
+    body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('my-theme', Theme.LIGHT);
+  }
+}
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarcup = menuItemTpl(menu);
+
+menuContainer.insertAdjacentHTML('beforeend', menuMarcup);
